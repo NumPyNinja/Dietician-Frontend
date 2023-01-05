982f44efb27ca64baa6b0636312489c1a7ace78d
Shenthamarai Selvi <shenthamarai.n@gmail.com>
2023-01-05 17:43:30 -0500

    validation, changes in component.ts

--- src/app/readpatients/readpatients.ts
+++ src/app/readpatients/readpatients.ts
@@ -1,2 +1,8 @@
 export class Readpatients {
+    userId?:number
+    firstName?:String;
+    lastName?:String
+    contact?:number
+    email?:String
+    fileName?:String
 }
--- src/app/readpatients/readpatients/readpatients.component.ts
+++ src/app/readpatients/readpatients/readpatients.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder , FormGroup, Validators} from '@angular/forms';
+import { ReadpatientsService } from '../readpatients.service';
 
 @Component({
   selector: 'app-readpatients',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./readpatients.component.css']
 })
 export class ReadpatientsComponent implements OnInit {
+  patientSearchForm !: FormGroup
 
-  constructor() { }
+  constructor(private readPatientsService :ReadpatientsService, private fb:FormBuilder) { }
 
   ngOnInit(): void {
+    this.patientSearchForm= this.fb.group({
+       Name : [''],
+       Email : ['',Validators.email],
+       PhoneNo :['',Validators.pattern('[0-9]{10,12}')]  /* include -+() in first set to include +,-,() in phone no validation */
+    })
   }
 
+  onSearchSubmit( patientSearchForm:FormGroup){
+    if(this.patientSearchForm.valid ){
+      console.log("In submit script ");
+      console.log(this.patientSearchForm.value);
+    }
+  }
 }
